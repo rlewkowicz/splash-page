03994f07204b039d1bc75637bf700813b2c4f29f
rlewkowicz <ryan.lewkowicz@spindance.com>
2016-11-17 17:59:31 -0500

    Now my jank css wont break on resize

--- resources/views/splash.php
+++ resources/views/splash.php
@@ -8,19 +8,23 @@
 </head>
     <body>
         <section class="main_section">
+            <div class="link link-jenkins">
+                <a href="http://jenkins.binaryoasis.com/">http://jenkins.binaryoasis.com/</a>
+            </div>
             <div class="main_div">
-                <a href="http://jenkins.binaryoasis.com/" class="link link-jenkins">http://jenkins.binaryoasis.com/</a>
                 <div class="squarerow">
                     <div class="squarecolumn">
                         <div class="griddiv">
                             <a class="gridleft" href='http://jenkins.binaryoasis.com/job/mediawiki-docker-phpfpm/'><img src='http://jenkins.binaryoasis.com/buildStatus/icon?job=mediawiki-docker-phpfpm'></a>
                             <a class="gridleft" href='https://hub.docker.com/r/rlewkowicz/parsoid/'>https://hub.docker.com/r/rlewkowicz/parsoid/</a>
+                            <a class="gridleft" href='https://github.com/rlewkowicz/docker-mediawiki-phpfpm/tree/master/7.0/alpine'>https://github.com/rlewkowicz/docker-mediawiki-phpfpm/tree/master/7.0/alpine</a>
                         </div>
                         <div class="griddiv gridcore"><img src="/assets/phpfpm.png" class="gridimage"> </img></div>
                         <div class="griddiv gridcore"><img src="/assets/parsoid.png" class="gridimage"> </img></div>
                         <div class="griddiv">
-                            <a class="gridright" href'http://jenkins.binaryoasis.com/job/mediawiki-docker-parsoid'><img src='http://jenkins.binaryoasis.com/job/mediawiki-docker-parsoid/badge/icon'></a>
+                            <a class="gridright" href='http://jenkins.binaryoasis.com/job/mediawiki-docker-parsoid/'><img src='http://jenkins.binaryoasis.com/buildStatus/icon?job=mediawiki-docker-parsoid'></a>
                             <a class="gridright" href='https://hub.docker.com/r/rlewkowicz/parsoid/'>https://hub.docker.com/r/rlewkowicz/parsoid/</a>
+                            <a class="gridright" href='https://github.com/rlewkowicz/docker-mediawiki-parsoid/tree/master/6.9/alpine'>https://github.com/rlewkowicz/docker-mediawiki-parsoid/tree/master/6.9/alpine</a>
                         </div>
                         <div class="griddiv">
 
